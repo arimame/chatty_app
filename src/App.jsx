12e53280e5e7c8f281f910234b90391dc8a9fbaf
Alissa Balge <alissa.balge@gmail.com>
2018-10-30 00:49:24 +0000

    addeding react components for navbar, footer and messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+      <Navbar />
+      <ChatBar />
+      <MessageList />
+      </div>
     );
   }
 }
+
+class Navbar extends Component {
+  render () {
+  return (
+  <div className = "navbar navbar-brand">Chatty</div>)
+  }
+}
+
 export default App;
